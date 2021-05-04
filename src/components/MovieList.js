2034b907cb875b77219d138f8beb0ec127c3e107
vikrambenipal <vikrambenipal@gmail.com>
2021-05-04 15:13:46 -0400

    connected to omdb api and am now trying to get list of movies from search result

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+
+
 
 const MovieList = ({ search }) => {
+
+    const [result, setResult] = useState([]);
+
+    axios.get(`http://www.omdbapi.com/?apikey=e82192d8&s=${search}`)
+    .then((e) => {
+        if(e.data.Response){
+            console.log(e.data.Search);
+        }else{
+            
+        }
+    })
+
     return (
         <div>
             <p>Results for <span>{search}</span></p>
+            {console.log(result)}
+            {result.map(movie => {
+                return <p>{movie.title}</p>
+            })}
         </div>
     )
 }
